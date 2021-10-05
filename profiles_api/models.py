c557690aa28bfb0354a1f1320d3c74c832d028c5
heethanjan <heethanjanheetha@gmail.com>
2021-10-05 15:54:25 +0530

    Added helloApiView

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -20,7 +20,7 @@ class UserProfileManager(BaseUserManager):
 
         return user
 
-    def create_super_user(self, email, name, password):
+    def create_superuser(self, email, name, password):
         """create and save anew super user"""
         user=self.create_user(email, name, password)
 
